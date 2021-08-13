f8d104bc8ef3bc4b4acde9f3e4cbf2526db0e36b
vernonranis <vernonranis@gmail.com>
2021-08-13 15:40:51 +0800

    finished with main functions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const difficultyRange = document.querySelector(".content-wrapper h3");
 const selectedDifficulty = document.querySelector(".drop-down");
 const inputField = document.querySelector("#guess");
-
+let secretNumber = Math.floor(Math.random()*10)+1;
+// console.log(secretNumber);
 
 function updateSelectedOption() {
   const currentInputOption = selectedDifficulty.value;
@@ -10,38 +11,60 @@ function updateSelectedOption() {
     case currentInputOption === "easy":
       difficultyRange.innerText = " 1 - 5 ";
       inputField.setAttribute("max", "5");
-
+      secretNumber = Math.floor(Math.random()*5)+1;
+      console.log(secretNumber);
       break;
 
     case currentInputOption === "normal":
       difficultyRange.innerText = " 1 - 10 ";
       inputField.setAttribute("max", "10");
+      secretNumber = Math.floor(Math.random()*10)+1;
+      console.log(secretNumber);
       break;
 
     case currentInputOption === "hard":
       difficultyRange.innerText = " 1 - 100 ";
       inputField.setAttribute("max", "100");
+      secretNumber = Math.floor(Math.random()*100)+1;
+      console.log(secretNumber);
       break;
 
     case currentInputOption === "impossible":
       difficultyRange.innerText = " 1 - 1000 ";
       inputField.setAttribute("max", "1000");
+      secretNumber = Math.floor(Math.random()*1000)+1;
+      console.log(secretNumber);
       break;
 
     default:
       console.log("I love Grace");
       break;
     }
-    console.log(inputField.getAttribute("max"));
+    console.log(secretNumber);
+    return(secretNumber);
+
 }
 
 inputField.addEventListener("input", () =>{
-  if (inputField.value <= 0) {
+  const userInput = parseFloat(inputField.value);
+  const maxUserInput = parseFloat(inputField.getAttribute("max"));
+
+  if (userInput <= 0) {
       alert("Input cannot be less than or equal to zero!");
       inputField.value = "";
   }
-  if (inputField.value > inputField.getAttribute("max")) {
-      alert("Input cannot be bigger than the limit: " + inputField.getAttribute("max"));
+  if (userInput > maxUserInput) {
+      alert("Input cannot be bigger than the limit: " + maxUserInput);
       inputField.value = "";
+  }
+});
+
+inputField.addEventListener("keypress", (e) =>{
+  if (e.key === "Enter") {
+    if (secretNumber === parseFloat(inputField.value)) {
+      console.log("You got it!");
+    } else {
+      console.log("wrong answer");
+    }
   }
 });
